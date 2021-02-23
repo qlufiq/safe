393a123ffed2544fcec4801070e1e52a177a61ae
qlufiq <42789637+qlufiq@users.noreply.github.com>
2021-02-23 21:29:06 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="https://safe.cara-ku.com/2021/02/6-easy-steps-for-bitcoin-tradin
 if(x=="2"){xxx="https://safe.cara-ku.com/2018/09/10-tips-for-start-small-business-startup.html"}
 if(x=="3"){xxx="https://safe.cara-ku.com/2018/09/the-way-to-start-business-at-home.html"}
 if(x=="4"){xxx="https://safe.cara-ku.com/2018/09/advantages-of-being-home-business-owner.html"}
-if(x=="5"){xxx="https://safe.cara-ku.com/2018/09/practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://safe.cara-ku.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="https://safe.cara-ku.com/2018/09/practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://safe.cara-ku.com//2018/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
